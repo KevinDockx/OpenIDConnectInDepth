85ebd185ceb484258c8dbba1e5ec62f10094a7d9
Dieter Goetelen <dieter.goetelen@hotmail.com>
2018-05-07 19:10:42 +0200

    feat: unsubscribe observables

--- src/Sample.AngularClient/src/app/signin-oidc/signin-oidc.component.ts
+++ src/Sample.AngularClient/src/app/signin-oidc/signin-oidc.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { OpenIdConnectService } from '../shared/open-id-connect.service';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { OpenIdConnectService } from "../shared/open-id-connect.service";
+import { Router } from "@angular/router";
+import { Subject } from "rxjs/Subject";
+import "rxjs/add/operator/takeUntil";
 
 @Component({
-  selector: 'app-signin-oidc',
-  templateUrl: './signin-oidc.component.html',
-  styleUrls: ['./signin-oidc.component.css']
+  selector: "app-signin-oidc",
+  templateUrl: "./signin-oidc.component.html",
+  styleUrls: ["./signin-oidc.component.css"]
 })
-export class SigninOidcComponent implements OnInit {
+export class SigninOidcComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe: Subject<void> = new Subject();
+  constructor(
+    private openIdConnectService: OpenIdConnectService,
+    private router: Router
+  ) {}
 
-  constructor(private openIdConnectService: OpenIdConnectService, 
-    private router: Router) { }
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 
   ngOnInit() {
-
-    this.openIdConnectService.userLoaded$.subscribe((userLoaded) => {
+    this.openIdConnectService.userLoaded$
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(userLoaded => {
         if (userLoaded) {
-            this.router.navigate(['./']);
-        }      
-    });
+          this.router.navigate(["./"]);
+        }
+      });
 
-    this.openIdConnectService.handleCallback();        
+    this.openIdConnectService.handleCallback();
   }
 }
