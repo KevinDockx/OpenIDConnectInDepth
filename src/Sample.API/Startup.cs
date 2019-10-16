5d813f07905d321d3c43b257834fd003cab36944
Kevin Dockx <kevin.dockx@gmail.com>
2019-10-16 10:41:04 +0200

    Switch to AddControllers instead of AddMvc (for APIs)

--- src/Sample.API/Startup.cs
+++ src/Sample.API/Startup.cs
@@ -20,7 +20,7 @@ namespace Sample.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers();
 
             services.AddCors();
 
--- src/Sample.SecondAPI/Startup.cs
+++ src/Sample.SecondAPI/Startup.cs
@@ -26,7 +26,7 @@ namespace Sample.SecondAPI
               options.ApiName = "samplesecondapi";
           });
             
-            services.AddMvc();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
